7a1200b638caf883681463c7bde2d1a3d91f999b
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2018-10-21 19:42:13 +0300

    [UPDATE] currency converter updated

--- parser.py
+++ parser.py
@@ -1,11 +1,23 @@
 import json
 from urllib.request import urlopen
+from pprint import pprint
 
 # Using https://currencylayer.com
-access_key = '{your access key}'
+# access_key = '{your access key}'
+access_key = 'access_key=your_access_key'
 currencies = 'currencies=EUR,GBP,CAD,PLN'
 source = 'source=USD'
 format = 'format=1'
-with urlopen("http://apilayer.net/api/live" + '?' + access_key + '&' + currencies + '&' + source + '&' + format) as response:
+with urlopen("http://apilayer.net/api/live" + '?' + access_key + '&' + currencies + '&' + source + '&' + format) \
+        as response:
     source = response.read()
-print(source)
+data = json.loads(source)
+pprint(json.dumps(data, indent=2))
+
+usd_rates = dict()
+for item in data['quotes']:
+    name = item
+    price = data['quotes'][str(item)]
+    usd_rates[name] = price
+
+pprint(50 * usd_rates['USDEUR'])
